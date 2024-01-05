c16b3cbf285850d1209c8f2a702f681534859dce
HimanshuSingh-08 <himansh.singh293@gmail.com>
2024-01-05 16:24:28 +0530

    solve findlargest

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,11 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let largestNum = numbers[0];
+    for(let i =1; i<numbers.length ; i++){
+        if(numbers[i]>largestNum) largestNum=numbers[i];
+    }
+    return largestNum;
 }
 
 module.exports = findLargestElement;
