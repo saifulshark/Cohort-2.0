f4214cc1be4c59022055b6d84f354a10a642ee0a
Hashit <raizadaharshit2004@gmail.com>
2024-03-07 17:46:42 +0530

    largest Element solved

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,16 @@
 */
 
 function findLargestElement(numbers) {
-    
+  let biggestNum = numbers[0];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (biggestNum < numbers[i]) {
+      biggestNum = numbers[i];
+    }
+  }
+  return biggestNum;
 }
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9.5, 10.3, 12.1, 6];
+// console.log(`Debug Statement: ${findLargestElement(arr)}`);
 
 module.exports = findLargestElement;
