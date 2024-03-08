358605e99f9fb1335a64d5e2e255834ff87b476e
Hashit <raizadaharshit2004@gmail.com>
2024-03-08 15:39:18 +0530

    ok

--- week-1/offline-class-1/level-2/01-Class.js
+++ week-1/offline-class-1/level-2/01-Class.js
@@ -1,12 +1,12 @@
-
 class Animal {
   constructor(name, legCount) {
-    this.name = name
-    this.legCount = legCount
+    this.name = name;
+    this.legCount = legCount;
   }
+
+  harshit = new Animal("Harshit", 4);
+
   describe() {
-    return `${this.name} has ${this.legCount} legs`
+    return `${this.name} has ${this.legCount} legs`;
   }
 }
-
-
