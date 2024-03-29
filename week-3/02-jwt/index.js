076cd1081e76f6796ae2de2a4400b66485c764e1
ruchir.saxena <ruchir.saxena@gmail.com>
2024-03-29 21:02:35 +0530

    Week-3 JWT Solution

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -31,10 +31,6 @@ function signJwt(username, password) {
     return token;
 }
 
-function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
 
 /**
  * Verifies a JWT using a secret key.
