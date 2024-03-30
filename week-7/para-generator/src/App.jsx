2ef7c58ef8d1977b842f237b49cbce56821194c4
vishnubv944 <vishnubv944@gmail.com>
2024-03-30 22:40:35 +0530

    updated para-generator

--- week-7/para-generator/src/App.jsx
+++ week-7/para-generator/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       <div class="flex flex-col items-center pt-28">
         <h1 class="text-5xl font-medium">Para Generator</h1>
         <div class="mt-7">
-          <input onChange={(e) => setWords(e.target.value)} class="h-14 sm:w-[750px] border border-gray-300 px-4 py-2 focus:outline-0" type="search" name="" placeholder="Enter the number of words" id="" />
+          <input required onChange={(e) => setWords(e.target.value)} class="h-14 sm:w-[750px] border border-gray-300 px-4 py-2 focus:outline-0" type="search" name="" placeholder="Enter the number of words" id="" />
           <button onClick={getWords} class="inset-y-0 right-0 ml-4 h-14 rounded-2xl bg-black px-4 py-2 text-white">Generate</button>
         </div>
         {
