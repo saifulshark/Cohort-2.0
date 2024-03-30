98ce137bd4896f41f94843ebc699dccc83fcce36
HashirAKB <ahmedhashir96@gmail.com>
2024-03-30 13:48:15 +0530

    introduced JWT secret as global variable.

--- week-3/04-mongo-with-jwt-auth/index.js
+++ week-3/04-mongo-with-jwt-auth/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const adminRouter = require("./routes/admin")
 const userRouter = require("./routes/user");
+const MY_SECRET_KEY = "HAKB";
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
@@ -13,3 +14,5 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = MY_SECRET_KEY;
