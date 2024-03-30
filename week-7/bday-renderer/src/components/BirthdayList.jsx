cc17d478dad40740198c7dfaddcdb5b458043f0a
vishnubv944 <vishnubv944@gmail.com>
2024-03-30 22:38:42 +0530

    made bday-renderer responsive and created a build

--- week-7/bday-renderer/src/components/BirthdayList.jsx
+++ week-7/bday-renderer/src/components/BirthdayList.jsx
@@ -9,9 +9,9 @@ export function BirthdayList(){
     
     return (
         <div className="flex flex-col items-center mt-16">
-            <div className="font-semibold text-6xl">Find Your Birthday Twin Here!</div>
+            <div className="font-semibold text-center text-4xl sm:text-6xl">Find Your Birthday Twin Here!</div>
 
-            <div className="mt-32 grid grid-cols-4 gap-4">
+            <div className="mt-5 sm:mt-32 grid sm:grid-cols-4 gap-4">
                 {
                     bdays.map((obj) => <BirthdayCard name={obj.name} dob={obj.dob} />)
                 }
--- week-7/bday-renderer/src/components/InputPage.jsx
+++ week-7/bday-renderer/src/components/InputPage.jsx
@@ -37,11 +37,11 @@ export default function InputPage(){
     return (
         <>
             <div className="flex items-center justify-center">
-                <div className="relative z-0 h-[600px] w-[100%] bg-[url('https://64.media.tumblr.com/0b82586e0100feed13cc9d39b10462dd/966121cf4541ca7a-ed/s1280x1920/330bd5dd23ccdfb9b1db3f5ac504d8e6e6a99434.jpg')] object-fill blur-sm" style={{backgroundSize: 'cover'}}></div>
-                <div className="absolute z-10 flex h-80 w-[60%] flex-col items-center justify-start rounded-2xl bg-gray-400 bg-opacity-60 shadow-2xl">
-                    <p className="mt-5 text-5xl font-medium">Tell us about your birthday!</p>
-                    <input onChange={handleName} type="text" className="mt-12 h-9 w-2/4 rounded text-center caret-slate-950" name="" placeholder="Enter Your Name" />
-                    <input onChange={handleBirthday} type="date" className="mt-7 h-9 w-2/4 rounded text-center" name="" placeholder="Enter Your Name" />
+                <div className="relative z-0 h-[400px] sm:h-[600px] w-[100%] bg-[url('https://wallpapercave.com/wp/wp9160769.jpg')] object-fill blur-sm" style={{backgroundSize: 'cover'}}></div>
+                <div className="absolute z-10 flex sm:h-80 w-[80%] sm:w-[60%] flex-col items-center justify-start rounded-2xl bg-gray-400 bg-opacity-60 shadow-2xl">
+                    <p className="mt-5 p-1 text-4xl text-center sm:text-5xl font-medium">Tell us about your birthday!</p>
+                    <input onChange={handleName} type="text" className="mt-12 h-9 sm:w-2/4 rounded text-center caret-slate-950" name="" placeholder="Enter Your Name" />
+                    <input onChange={handleBirthday} type="date" className="mt-7 h-9 sm:w-2/4 rounded text-center" name="" placeholder="Enter Your Name" />
                     <button onClick={addData} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 mt-8">Add</button>
                 </div>
             </div>
