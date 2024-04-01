fb0556fde448885efa472f94dc0db19805881786
Rohan Mathur <rm30092000mr@gmail.com>
2024-04-02 00:14:55 +0530

    Solved Hard of 01-js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,39 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0
+  }
+  add(num) {
+    this.result += num
+  }
+  subtract(num) {
+    this.result -= num
+  }
+  multiply(num) {
+    this.result *= num
+  }
+  divide(num) {
+    if (num === 0) {
+      throw new Error("Cannot divide by 0")
+    }
+    this.result /= num
+  }
+  clear() {
+    this.result = 0
+  }
+  getResult() {
+    return this.result
+  }
+  calculate(str) {
+    let ans = eval(str)
+    if (ans === Infinity) {
+      throw new Error("Cannot divide by 0")
+    }
+    this.result = ans
+    return this.result
+  }
+}
 
 module.exports = Calculator;
