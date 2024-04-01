65080e567459e85eba437492accb66759e6d6b03
Rohan Mathur <rm30092000mr@gmail.com>
2024-04-02 00:56:13 +0530

    Added functions for Easy -> 01-async-js

---
+++ week-2/01-async-js/easy/2-counter.js
@@ -0,0 +1,88 @@
+// ## Counter without setInterval
+
+// Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+
+
+
+let count=0; 
+function increment(){ 
+    count++; 
+    console.log(count) 
+    setTimeout(increment,1000) 
+} 
+setTimeout(increment,1000)
+
+console.log(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (Hint: setTimeout)
