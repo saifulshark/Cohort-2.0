1a7bc6f16dd2ceb9e26c0768fec6368b01121363
HarshalShinde03 <harsh.shinde70@gmail.com>
2024-04-02 15:29:38 +0530

    Update 4-promise-chain.js
    Solved the problem

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,32 @@
  */
 
 function wait1(t) {
-
+    return new Promise((res)=>{
+        setTimeout(res,t*1000)
+    })
 }
 
 function wait2(t) {
-
+return new Promise((res)=>{
+        setTimeout(res,t*1000)
+    })
 }
 
 function wait3(t) {
-
+return new Promise((res)=>{
+        setTimeout(res,t*1000)
+    })
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const startTime = Date.now();
+    
+    return Promise.allSettled([wait1(t1),wait2(t2),wait3(t3)])
+    .then(()=>{
+        const endTime2 = Date.now();
+        return endTime2-startTime;
+    })
+    
 }
 
 module.exports = calculateTime;
