aa328b5a5667610e581c77395d010832a7f6e8ca
Hashit <raizadaharshit2004@gmail.com>
2024-04-02 20:13:51 +0530

    04-mongo middlewares reworked admin

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,25 +1,27 @@
 const { Admin } = require("../db");
+const { JWT_SECRET } = require("../config");
+const jwt = require("jsonwebtoken");
 
 // Middleware for handling auth
 async function adminMiddleware(req, res, next) {
   // Implement admin auth logic
   // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
-  const username = req.headers.username;
-  const password = req.headers.password;
+  const token = req.headers.authorization; //bearer token
+  const words = token.split(" "); //["Bearer", "token"]
+  const jwtToken = words[1]; //token
 
-  if (!password || !username) {
-    return res
-      .status(401)
-      .send("Unauthorized: Username and password are required in headers");
-  }
-  const alreadyExist = await Admin.findOne({
-    username: username,
-  });
-  if (alreadyExist) {
-    next();
-  } else {
-    res.status(403).json({
-      msg: "Admin already exist",
+  try {
+    const decodedValue = jwt.verify(jwtToken, JWT_SECRET);
+    if (decodedValue.username) {
+      next();
+    } else {
+      res.status(403).json({
+        msg: "You are not authenticated",
+      });
+    }
+  } catch (err) {
+    res.json({
+      msg: "Incorrect inputs",
     });
   }
 }
