7eb8c72777f4aa4deab7320c0b5ee31b31687f23
Rohan Mathur <rohan@icustomer.ai>
2024-04-05 08:53:16 +0530

    Solved Mongo Problems

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -2,21 +2,49 @@ const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
 
+const { User, Course } = require('../db')
+
 // User Routes
 router.post('/signup', (req, res) => {
-    // Implement user signup logic
+    const { username, password } = req.body
+    User.create({
+        username,
+        password
+    })
+    res.json({ msg: "User Created Successfully" })
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get('/courses', userMiddleware, async (req, res) => {
+    const courses = await Course.find({})
+    res.json({ courses })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
+    const { username } = req.headers
+    const { courseId } = req.params
+    await User.updateOne({
+        username
+    }, {
+        "$push": {
+            purchasedCourses: courseId
+        }
+    })
+    res.json({ msg: "Purchase Complete" })
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
+    const { username } = req.headers
+    const user = await User.findOne({
+        username
+    })
+    console.log(user.purchasedCourses)
+    const courses = await Course.find({
+        _id: {
+            "$in": user.purchasedCourses
+        }
+    })
+    res.json({ courses })
 });
+
 
 module.exports = router
