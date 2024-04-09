c63e38313b41e694981f0ca6a1bd38b37f5766ef
PrinceSachan <sachanprince096@gmail.com>
2024-04-09 18:54:57 +0530

    Solved week-4 problem

--- week-4/react-basics/hard/todo/src/pages/Todos.tsx
+++ week-4/react-basics/hard/todo/src/pages/Todos.tsx
@@ -7,7 +7,7 @@ const Todos = ({todos}) => {
         <div>
             {todos ? todos.map((todo) => {
                 return (
-                    <div className='mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5' key={todo.id}>
+                    <div className='mb-2 ~bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5' key={todo.id}>
                         <div className='mb-2'>
                             <p className='text-sm font-medium text-gray-900'>Title: </p>
                             <p>{todo.title}</p>
