442ca696789866621c6e6fec5a2ae4fd9b367ff8
HashirAKB <ahmedhashir96@gmail.com>
2024-04-12 13:13:15 +0530

    Week-6-practice
    -Initial commit.

---
+++ week-6/practice/week-6-practice/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <Header title="Hashir"/>
+      <Header tittle="Sameena"/>
+    </div>
+  )
+}
+
+function Header({title}){
+  return <div>
+    {title}
+  </div>
+}
+
+
+export default App
