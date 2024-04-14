725b4d4ed7f0e39905d3fe18833780d5e9a766fa
Himanshu Singh <himansh.singh293@gmail.com>
2024-04-14 12:06:19 +0530

    Implemented routes for admin

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,46 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    // Extract the data and add to the database
+    const {username , password} = req.body;
+    await Admin.create({
+        username,
+        password
+    })
+
+    res.status(200).json({
+        msg : "Admin created succesfully !!!"
+    })
+    
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const {title , description ,price,imageLink} = req.body;
+
+    await Course.create({
+        title,
+        description,
+        price,
+        imageLink
+    })
+    res.status(200).json({
+        msg : "Course Created Successfully !!!"
+    })
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    const courses = await Course.find({});
+
+    res.status(200).json({
+        courses
+    })
 });
 
 module.exports = router;
