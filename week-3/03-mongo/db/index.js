336f723cf28658f1f0a015dbf261ddd0bf039235
Himanshu Singh <himansh.singh293@gmail.com>
2024-04-14 12:00:30 +0530

    Created schema for User , Courses and Admin

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb://localhost:27017');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username : {type : String , required : true},
+    password : {type: String , required : true}
+
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username : {type : String , required : true},
+    password : {type : String , required : true},
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title:{type : String , required : true},
+    description : {type : String , required: true},
+    price : {type : Number , required : true},
+    imageLink : {type : String}
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
