7a03d5d47c99121ce4c3851e76e91a4760c2f41f
Himanshu Singh <himansh.singh293@gmail.com>
2024-04-14 12:02:19 +0530

    Implemented middlewares for user and Admin

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,24 @@
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+const { Admin } = require("../db");
+//First validate 
+
+
+async function adminMiddleware  (req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+    const {username , password} = req.headers;
+
+    Admin.findOne({username , password}).then(function(value){
+        if(value){
+            next();
+        }else{
+            res.status(403).json({
+                msg : "Admin Does not exist!!!"
+            })
+        }
+    })
+
 }
 
 module.exports = adminMiddleware;
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,27 @@
+const { User } = require('../db');
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const { username, password } = req.headers;
+    // we can also use the async await method for this.
+    User.findOne({
+        username,
+        password
+    }).then(function (value) {
+        if (value) {
+            next();
+        } else {
+            res.status(403).json({
+                msg: "User doesnt exists"
+            })
+        }
+    })
+
 }
+
+//Note : always remeber to call next middleware using next() else it will get stuck
+
 
 module.exports = userMiddleware;
