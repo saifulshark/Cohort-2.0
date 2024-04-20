bb90653cfacdea22016d3c42a9715869942702ff
Shoaib Majid <126547607+ShoaibMajidDar@users.noreply.github.com>
2024-04-20 14:03:03 +0530

    solution of palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,7 +4,44 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  // changing the string to lower case
+  str = str.toLowerCase();
+
+  // removing spaces
+  str = (str.split(" ")).join("")
+
+  // removing punctuations
+  str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g,"")
+
+  // storing the length of str
+  const strlength = str.length;
+
+  //  return true for empty string
+  if (strlength==0){ return true}
+
+  else{
+
+    // initiating a pointer j that points to the last character of the string
+    let j = strlength-1;
+
+    // starting a loop
+    for (let i = 0; i<strlength; i++){
+
+      // for odd palindrome if i and j are equal then return true and for even palindrome if i > j return true
+      if (i>=j){
+        return true;
+      }
+
+      // if character at i index is equal to j decreament j and increament i, else return false
+      if (str[i]==str[j]){
+        j--;
+      }
+      else {
+        return false;
+      }
+    }
+  }
+
 }
 
 module.exports = isPalindrome;
