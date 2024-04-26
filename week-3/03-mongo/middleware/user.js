2899b2085ab7827aca051a9091d099f79a8aa550
Shahid Khan <shahidseran786@gmail.com>
2024-04-27 01:42:57 +0530

    Middlewares setup

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,18 @@
-function userMiddleware(req, res, next) {
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const username = req.headers.username;
+  const password = req.headers.password;
+
+  try{
+  const userFound = await Admin.findOne({ username, password });
+  if(adminFound)
+   next();
+  else
+   res.status(403).json({message:"User not found"})
+  }catch(err){
+    res.status(404).json({ Error: "Error to get user" });
+  }
 }
 
 module.exports = userMiddleware;
