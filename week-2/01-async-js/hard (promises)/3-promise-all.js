cd1746d80bca08b0505511adf8ae51c43ce6efce
Shoaib Majid <126547607+ShoaibMajidDar@users.noreply.github.com>
2024-04-27 11:00:17 +0530

    solution promisify all

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,31 @@
  */
 
 function wait1(t) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, t)
+    });
 
 }
 
 function wait2(t) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, t)
+    });
 
 }
 
 function wait3(t) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, t)
+    });
 
 }
 
 function calculateTime(t1, t2, t3) {
-
+    let startTime = new Date().getTime();
+    return Promise.all([wait1(t1*1000), wait2(t2*1000), wait3(t3*1000)]).then(function(){
+        return new Date().getTime() - startTime
+    });
 }
 
 module.exports = calculateTime;
