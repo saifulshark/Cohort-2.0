65691623c118235d6750993f14d7804c751f495b
Shahid Khan <shahidseran786@gmail.com>
2024-04-28 23:29:52 +0530

    admin Routes Completed

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -22,28 +22,34 @@ router.post('/signup',async (req, res) => {
     } catch (error) {
         res.status(404).json({ Error: "Error in Creating Admin",error });
     }
-    
-
 });
 
 router.post('/courses', adminMiddleware,async (req, res) => {
     // Implement course creation logic
     try {
-    const { title, description, price, imageLink, published } = { ...req.body };
-    await Course.create({title, description, price, imageLink, published})
+    const { title, description, price, imageLink, published,id } = { ...req.body };
+    await Course.create({id,title, description, price, imageLink, published})
      res.json({
        message: "Course created successfully",
-       courseId: "new course id",
+       courseId: id,
      });
     } catch (error) {
-        res.status(404).json({Error:"Error in creating Course"})
+        res.status(404).json({Error:"Error in creating Course",error})
     }
-    
-
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware,async (req, res) => {
     // Implement fetching all courses logic
+    try {
+      const allCourses = await Course.find()
+      if(allCourses)
+       res.json({ courses :allCourses});
+      else
+        res.status(404).json({Error:"Not get Any Courses"});
+    } catch (error) {
+      res.status(404).json({ Error: "Error to get Courses", error });
+    }
+
 });
 
 module.exports = router;
