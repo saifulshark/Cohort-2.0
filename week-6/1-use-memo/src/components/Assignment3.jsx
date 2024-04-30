11d1aa91aeb65ca9c9204b581303c8ad2400a642
HashirAKB <ahmedhashir96@gmail.com>
2024-04-30 13:43:53 +0530

    Completed assignment 3.

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -8,11 +8,19 @@ export const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 100 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    // const totalValue = 0;
+    const totalValue = useMemo(() => {
+        let sum = 0;
+        items.map((item, index) => {
+            sum = sum + item.value;
+        })
+        return sum;
+    },[items]);
     // Your code ends here
     return (
         <div>
