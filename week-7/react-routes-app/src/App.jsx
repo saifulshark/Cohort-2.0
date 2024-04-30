d1829595f9fcb5cd8f05a4a9feb9d4ea6a7248bf
HashirAKB <ahmedhashir96@gmail.com>
2024-04-30 20:05:42 +0530

    Correct way to implement routes

--- week-7/react-routes-app/src/App.jsx
+++ week-7/react-routes-app/src/App.jsx
@@ -2,20 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import { Dashboard } from './components/Dashboard'
 import { Landing } from './components/Landing'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
     <>
-    <div /*style={{background: "black", color: "white"}}*/>
-      <button onClick={() => {window.location.href = "/";}}>Home</button>
-      <button onClick={() => {window.location.href = "/Dashboard";}}>Dashboard</button>
-    </div>
       < BrowserRouter>
+      <TopBar/>
         <Routes>
           <Route element={<div>Hi There</div>}/>
           <Route path='/Dashboard' element={<Dashboard/>}/>
@@ -23,6 +19,16 @@ function App() {
         </Routes>
     </BrowserRouter>
     </>
+  )
+}
+
+const TopBar = () => {
+  const navigate =useNavigate();
+  return (
+    <div /*style={{background: "black", color: "white"}}*/>
+      <button onClick={() => {navigate('/')}}>Home</button>
+      <button onClick={() => {navigate('/Dashboard')}}>Dashboard</button>
+    </div>
   )
 }
 
