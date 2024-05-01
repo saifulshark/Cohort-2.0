30145c6043b40a07cb7d785cb5648d6b1051c09f
HarshalShinde03 <harsh.shinde70@gmail.com>
2024-05-01 09:22:45 +0530

    Week 3 - Middleware

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,21 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function(req,res,next){
+  let userId = req.headers['user-id'];
+
+  if(numberOfRequestsForUser[userId]){
+    if(numberOfRequestsForUser[userId]>5){
+      res.status(404).send('no entry')
+    }else{
+      next();
+    }
+  }else{
+    numberOfRequestsForUser[userId] = 1
+    next();
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
