6173d24f0550ef32d87515317ac1bafe1ee1e917
Himanshu Singh <himansh.singh293@gmail.com>
2024-05-01 18:19:18 +0530

    Solved useMemo assignment 2

--- week-6/1-use-memo/src/components/Assignment2.jsx
+++ week-6/1-use-memo/src/components/Assignment2.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 // In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items. 
 // The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
 // You will learn something new here, specifically how you have to pass more than one value in the dependency array
+
+// this code is just to make the random sentences 
 
 const words = ["hi", "my", "name", "is", "for", "to", "random", "word" ];
 const TOTAL_LINES = 1000;
@@ -20,7 +22,10 @@ export function Assignment2() {
     const [sentences, setSentences] = useState(ALL_WORDS);
     const [filter, setFilter] = useState("");
 
-    const filteredSentences = sentences.filter(x => x.includes(filter))
+
+    const filteredSentences = useMemo(()=>{
+          return sentences.filter(x => x.includes(filter));
+    }, [sentences, filter]) ;// to handle this we can use the useMemo
 
     return <div>
         <input type="text" onChange={(e) => {
@@ -31,3 +36,13 @@ export function Assignment2() {
         </div>)}
     </div>
 }
+
+
+//note of learning why we used the two states
+// sentences : These have the information about the all sentences that are stored.
+// filter : This state is basically a variable that we are going to use to filter out
+//          the to search there for we have .
+
+// working flow : We basically use that filter value to use the filter criteria to find out the desired output.
+
+// useMemo how it is helping lets see : 
