64f6e3520577871d7e33fe7cd549dfd8d088f0e5
HarshalShinde03 <harsh.shinde70@gmail.com>
2024-05-03 09:59:50 +0530

    Week 3 - 03 04 assignment completed

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,18 +1,65 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const {Admin,User,Course} = require('../db')
 const router = Router();
+const {JWT_SECRET} = require('../config');
+const jwt = require('jsonwebtoken');
+
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    await Admin.create({
+        username,
+        password
+    })
+    res.status(200).json({
+        message: "Admin created successful"
+    });
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async(req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await Admin.find({
+        username,
+        password
+    })
+    if(user){
+        const token = jwt.sign({
+            username
+        },JWT_SECRET)
+        res.json({
+            token
+        })
+    }else{
+        res.status(403).json({
+            msg : "Incorrect Username or password"
+        })
+    }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async(req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+    // zod
+    const newCourse = await Course.create({
+        title,
+        description,
+        imageLink,
+        price
+    })
+
+    res.json({
+        message: 'Course created successfully', courseId: newCourse._id
+    })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
