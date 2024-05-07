0732127bcae4a14918bde2e8c0b6fc29689334be
tanishq-arya <tanishqarya24@gmail.com>
2024-05-07 21:33:24 +0530

    corrected solution

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -2,35 +2,26 @@
  * Write a function that halts the JS thread (make it busy wait) for a given number of milliseconds.
  * During this time the thread should not be able to do anything else.
  * the function should return a promise just like before
- */
-/*
-function sleep (seconds) {
-  return new Promise(function(resolve){
-    setTimeout(function(){
-      resolve(seconds +" seconds passed.");
-    }, seconds*1000);
-  });
-}
-
-async function main(seconds) {
-  console.log("Before await call");
-  const value = await sleep(seconds);
-  // halts the thread here for 'seconds' seconds
-  console.log("After await call");
-  console.log("Resolve value: " + value);
-}
-
-console.log("Before fn call");
-main(5);
-console.log("After fn call");
 */
 
 function sleep(milliseconds) {
-  return new Promise(function(resolve){
-    setTimeout(function(){
-      resolve();
-    }, milliseconds);
+  return new Promise(resolve => {
+    const startTime = Date.now();
+    // console.log("before loop");
+    while (Date.now() - startTime < milliseconds) {
+      // Busy-wait loop
+    }
+    resolve();
+    // setTimeout(resolve, milliseconds);
+    // console.log("after loop");
   });
 }
-
+/*
+// Example usage:
+console.log("Start");
+sleep(5000).then(() => {
+  console.log("Thread halted for 3 seconds");
+});
+console.log("End");
+*/
 module.exports = sleep;
