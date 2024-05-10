779d187e73b38f8cc0d0bd71466b8b50d3320785
HashirAKB <ahmedhashir96@gmail.com>
2024-05-10 23:01:45 +0530

    Stored notification state variables as atoms using recoil.

---
+++ week-7/7.4-master/vite-project/src/atoms.js
@@ -0,0 +1,27 @@
+import { atom } from 'recoil';
+
+export const networkAtom = atom({
+    key: "networkAtom",
+    default: 104
+})
+
+export const jobAtom = atom({
+    key: "jobAtom",
+    default: 0
+})
+
+export const messagingAtom = atom({
+    key: "messagingAtom",
+    default: 12
+})
+
+
+
+export const meAtom = atom({
+    key: "meAtom",
+    default: 0
+})
+
+
+
+
