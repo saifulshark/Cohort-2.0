4bda769cb6027911fa1a8655bc583f37ef0a9e3b
HashirAKB <ahmedhashir96@gmail.com>
2024-05-11 20:08:46 +0530

    Understood selectorFamily of recoil.

--- week-7/7.4-master/selector-family/src/App.jsx
+++ week-7/7.4-master/selector-family/src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   return <RecoilRoot>
     <Todo id={1}/>
     <Todo id={2} />
+    {/* Even if we add multiple components with same id, as we're using recoil and recoil selector family the fetch/xhr request will be sented to server only once (basically only for the first one). The rest will be given the value which is returned from the very first call/Request */}
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
   </RecoilRoot>
 }
 
