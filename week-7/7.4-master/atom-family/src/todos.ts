0d3cc571159b692250047ef06d15434a5c9f3ed8
HashirAKB <ahmedhashir96@gmail.com>
2024-05-11 19:35:40 +0530

    learned 'atomFamily'

--- week-7/7.4-master/atom-family/src/todos.ts
+++ week-7/7.4-master/atom-family/src/todos.ts
@@ -4,6 +4,14 @@ export const TODOS = [{
     description: "Hit the gym from 7-9"
 }, {
     id: 2,
+    title: "Take a bath",
+    description: "Hot water bath"
+},  {
+    id: 3,
     title: "Go to eat food",
     description: "Eat food from from 9-11"
+}, {
+    id: 4,
+    title: "Start coding",
+    description: "Do coding from from 11-02"
 },]
