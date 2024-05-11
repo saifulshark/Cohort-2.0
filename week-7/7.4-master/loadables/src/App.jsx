4f2b0d58ca81b3d6da576f5fa02f71c8b8bcb853
HashirAKB <ahmedhashir96@gmail.com>
2024-05-11 20:50:44 +0530

    understood 'useRecoilStateLoadable'.

--- week-7/7.4-master/loadables/src/App.jsx
+++ week-7/7.4-master/loadables/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
 }
 
 function Todo({id}) {
+  
+// useRecoilStateLoadable is a hook provided by Recoil, a state management library for React. It allows you to interact with Recoil state atoms in a way that supports asynchronous loading. With this hook, you can access the current state of a Recoil atom along with its loading state, error state, and any data it may contain. This is particularly useful for handling asynchronous data fetching and updating UI components accordingly. By utilizing useRecoilStateLoadable, you can create more responsive and efficient React applications that manage complex state asynchronously.
    const [todo, setTodo] = useRecoilStateLoadable(todosAtomFamily(id));
    if (todo.state === "loading") {
       return <div>loading</div>
