53eac6c57eb365aadc9026b0a85da777817bfc7c
HashirAKB <ahmedhashir96@gmail.com>
2024-05-16 21:44:53 +0530

    useTodos custom react hook.

---
+++ week-9 - TS and Hooks/gists/useTodos_custom_react_hook.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const useTodos = () => {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos);
+      })
+  }, [])
+
+  return todos;
+}
+
+function App() {
+  const todos = useTodos();
+  return (
+    <>
+      {todos.map(todo => <Track todo={todo} />)}
+    </>
+  )
+}
+
+function Track({ todo }) {
+  return <div>
+    {todo.title}
+    <br />
+    {todo.description}
+  </div>
+}
+
+export default App
