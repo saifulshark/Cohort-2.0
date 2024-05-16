b068a63be36b895e7f79a5739c14a80200c98c39
HashirAKB <ahmedhashir96@gmail.com>
2024-05-16 21:52:58 +0530

    loading message for better UE.

--- week-9 - TS and Hooks/customHooks/src/App.jsx
+++ week-9 - TS and Hooks/customHooks/src/App.jsx
@@ -2,23 +2,32 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const useTodos = () => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get("https://sum-server.100xdevs.com/todos")
       .then(res => {
         setTodos(res.data.todos);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error("error fetching todos.");
+        setLoading(false);
       })
   }, [])
 
-  return todos;
+  return {
+    todos: todos,
+    loading: loading,
+  };
 }
 
 function App() {
-  const todos = useTodos();
+  const {todos, loading} = useTodos();
   return (
     <>
-      {todos.map(todo => <Track todo={todo} />)}
+      {loading ? <div> Loading...</div> : todos.map(todo => <Track todo={todo} />)}
     </>
   )
 }
