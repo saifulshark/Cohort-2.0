9899021e596650c475dd3408504ab1e04ce67d89
aman154 <fakex1276@gmail.com>
2024-05-18 19:19:01 +0530

    times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,20 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
-}
+    // Start time
+    const startTime = new Date().getTime();
+  
+    // Compute sum from 1 to n
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+  
+    // End time
+    const endTime = new Date().getTime();
+  
+    // Calculate time difference in seconds
+    const timeDifference = (endTime - startTime) / 1000;
+  
+    return timeDifference;
+  }
