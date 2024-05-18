e9c1e67a74c7832c809e683a80a60a244d99520c
0Rohit2022 <rohitkumarrr2022@gmail.com>
2024-05-18 22:51:39 +0530

    first commit

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,22 @@
 */
 
 function findLargestElement(numbers) {
-    
+  if (numbers.length === 0) {
+    throw new Error("Array is Empty");
+  }
+
+  let largest = numbers[0];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if(largest < numbers[i])
+        {
+            largest = numbers[i];
+        }
+  }
+  return largest;
 }
+
+const numbers = [3, 7, 2, 9, 1];
+console.log(findLargestElement(numbers));
 
 module.exports = findLargestElement;
