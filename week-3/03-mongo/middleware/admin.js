1f7fb3328235ed402be9341c6549571a42ab6470
amandev200 <amansinghdev200@gmail.com>
2024-05-23 00:32:46 +0530

    Done the MongoDb Assignment without jwt

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -2,6 +2,20 @@
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const username = req.headers.username;
+    const password = req.headers.password;
+    Admin.findOne({
+        username :username,
+        password :password,
+    }).then(function(value){
+        if(value){
+            next();
+        }
+        else
+        {
+            res.status(404).json("Admin Does not exist")
+        }
+    })
 }
 
 module.exports = adminMiddleware;
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,36 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Course, Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
+    const username = req.headers.username;
+    const password = req.headers.password;
+    Admin.create({
+        username,
+        password
+    })
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+    const coursetitle = req.body.coursetitle;
+    const coursedescription = req.body.coursedescription;
+    const courseprice = req.body.courseprice;
+    const imagelink = req.body.imagelink;
+    Course.create({
+        coursetitle,
+        coursedescription,
+        courseprice,
+        imagelink
+    })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    res.send(Course.find({}));
 });
 
 module.exports = router;
