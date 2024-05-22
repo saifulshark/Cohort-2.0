464fa045a798003b69340412f43eb90a7da957ee
Shahid Khan <shahidseran786@gmail.com>
2024-05-23 00:51:23 +0530

    Week-6 (useMemo)

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -1,8 +1,11 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useRef } from 'react';
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
 export const Assignment3 = () => {
+    const itemName=useRef();
+    const itemPrice=useRef();
+    const [demoValue, setDemoValue] = useState("");
     const [items, setItems] = useState([
         { name: 'Chocolates', value: 10 },
         { name: 'Chips', value: 20 },
@@ -10,18 +13,40 @@ export const Assignment3 = () => {
         { name: 'Tomato', value: 30 },
         // Add more items as needed
     ]);
-
+    const totalPrice=()=>{
+        console.log("Rendering")
+        return items.reduce((com, item) => com + item.value, 0);
+    }
     // Your code starts here
-    const totalValue = 0;
+    // const totalValue =totalPrice();
+      
+    
+    const totalValue = useMemo(() => {
+        console.log("Use memo Re-rendering")
+      return totalPrice();
+    }, [items]);
     // Your code ends here
     return (
-        <div>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item.name} - Price: ${item.value}</li>
-                ))}
-            </ul>
-            <p>Total Value: {totalValue}</p>
+      <div>
+        <div className="addItem">
+          <input type="text" placeholder="Item name" ref={itemName} />
+          <input type="text" placeholder="Item Price" ref={itemPrice} />
+          <input type="text" placeholder="Check" value={demoValue} onChange={(e)=>setDemoValue(e.target.value)} />
+          <button onClick={()=>{
+            setItems(prev=>[...prev,{
+                name:itemName.current.value,
+                value:Number(itemPrice.current.value)
+            }])
+          }}>ADD</button>
         </div>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item.name} - Price: ${item.value}
+            </li>
+          ))}
+        </ul>
+        <p>Total Value: {totalValue}</p>
+      </div>
     );
 };
