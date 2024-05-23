75552e608932322c2c8a7e74448832feefc6ee0b
prince981620 <prince981620@gmail.com>
2024-05-24 02:24:57 +0530

    solution for week-2/01-async-js/hard(promises)

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,42 @@
  */
 
 function wait1(t) {
-
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },t*1000)
+    })
 }
 
 function wait2(t) {
-
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },t*1000)
+    })
 }
 
 function wait3(t) {
-
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve();
+        },t*1000)
+    })
+}
+function promise_all(t1,t2,t3){
+        const time1 = wait1(t1);
+        const time2 = wait2(t2);
+        const time3 = wait3(t3);
+        return Promise.all([time1,time2,time3]);
 }
 
-function calculateTime(t1, t2, t3) {
-
+async function calculateTime(t1, t2, t3) {
+    const initDate = new Date();
+    const initTime = initDate.getTime();
+    await promise_all(t1,t2,t3);
+    const finDate = new Date();
+    const finTime = finDate.getTime();
+    return finTime - initTime;
 }
 
 module.exports = calculateTime;
