e1500ee04f2f32f788dc7528da5fb36d70582a5c
madhav-relish <madhavsingh.relish@gmail.com>
2024-05-25 13:25:07 +0530

    fileServer Solution + jest setup issue for windows
    - FileServer assignment submission
    - Run test command changes for windows

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,10 +12,34 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const app = express();
-
-
-module.exports = app;
+    const express = require("express");
+    const fs = require("fs");
+    const path = require("path");
+    const app = express();
+    
+    app.get("/files/", function (req, res) {
+      fs.readdir(path.join(__dirname, "./files/"), (err, files) => {
+        if (err) {
+          return res.status(500).json({ error: "Failed to retrieve files" });
+        }
+        res.json(files);
+      });
+    });
+    
+    app.get("/file/:fileName", function (req, res) {
+      const fileName = req.params.fileName;
+      const filePath = path.join(__dirname, "./files/", fileName);
+      fs.readFile(filePath, "utf8", (error, data) => {
+        if (error) {
+          res.status(404).send("File not found");
+        }
+        res.send(data);
+      });
+    });
+    
+    app.all("*", (req, res) => {
+      res.status(404).send("Route not found");
+    });
+    
+    module.exports = app;
+    
