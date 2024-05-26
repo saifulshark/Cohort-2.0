2f6d9397b71fc37f29d009f4f0d3c4cafdbe13c3
Gauravpadda <gauravpadda98@gmail.com>
2024-05-26 18:31:21 +0530

    todo list simple website

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -4,7 +4,9 @@ function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
     try{
-        const token=req.headers.authorization
+        const jwttoken=req.headers.authorization
+        const word=jwttoken.split(" ")
+        const token=word[1]
         const verification=jwt.verify(token,jwtPassword)
         next();
     }catch{
