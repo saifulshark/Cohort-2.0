322ddf8422c606ad615ad74635bf3a51ab6ea049
prince981620 <prince981620@gmail.com>
2024-05-27 17:35:44 +0530

    solution for 02-reatelimitter

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,21 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req,res,next)=>{
+  const userID = req.header["user-id"];
+  if(numberOfRequestsForUser.userID){
+    numberOfRequestsForUser.userID++;
+    if(numberOfRequestsForUser.userID > 5){
+      res.status(404).send("No Entry");
+    }else{
+      next();
+    }
+  }else{
+    numberOfRequestsForUser.userID = 1;
+    next();
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
