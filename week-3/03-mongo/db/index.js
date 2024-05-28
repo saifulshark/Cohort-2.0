0c9cef3064929f4de77d65f656c64323f8c7f775
barathraju-43 <barath11raju@gmail.com>
2024-05-28 23:58:44 +0530

    mongo assignment completed

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
+// const z = require('zod');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://admin:barath43@cluster0.jz678vk.mongodb.net/');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+        courseId: Number,
+        title: String,
+        description: String,
+        Price: Number,
+        imageLink: String,
+        published: Boolean
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
