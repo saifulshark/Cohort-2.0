97ac06a9019d912ed7bd3f6d20b92b015c7fcc99
prince981620 <prince981620@gmail.com>
2024-05-29 14:06:30 +0530

    solved week-3/03-mongo

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://prince981620:Hello%4012345@cluster0.twgxb.mongodb.net/course');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    prince: Number
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,54 @@
 const { Router } = require("express");
+// const express = require("express");
+
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
-
+// router.use(express.json());
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const newAdmin = new Admin({
+        username: username,
+        password: password
+    });
+    await Admin.create(newAdmin);
+    res.json({
+        msg: "Admin created"
+    })
+
+
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async(req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+
+    const newCourse = new Course({
+        title: title,
+        description: description,
+        imageLink: imageLink,
+        price: price
+    })
+    await Course.create(newCourse);
+    res.json({
+        msg: "course created"
+    })
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get('/courses', adminMiddleware, async(req, res) => {
+    // Implement fetching all courses laogic
+    const allCourses = await Course.find({});
+    res.json({
+        courses: allCourses
+    })
+
 });
 
 module.exports = router;
