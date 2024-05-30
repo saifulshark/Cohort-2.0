8f3f8c39cc9a6dca52af7618d672725f3d409043
HashirAKB <ahmedhashir96@gmail.com>
2024-05-30 19:14:35 +0530

    improved login page.

--- week-12/todo-Mono-repo/client/src/Components/Login.tsx
+++ week-12/todo-Mono-repo/client/src/Components/Login.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
+import {LoginParams} from '@hashirakb/common';
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const handleLogin = async () => {
+        const loginParams: LoginParams = {username, password};
         const response = await fetch('http://localhost:3000/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username, password })
+            body: JSON.stringify(loginParams)
         });
         // Todo: Create a type for the response that you get back from the server
         const data = await response.json();
