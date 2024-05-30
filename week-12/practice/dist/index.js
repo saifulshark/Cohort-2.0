754ee399deae4443fbed9913819932b3ece151f1
HashirAKB <ahmedhashir96@gmail.com>
2024-05-30 15:07:16 +0530

    Understood TS Pick and Partial

--- week-12/practice/dist/index.js
+++ week-12/practice/dist/index.js
@@ -1,14 +1,35 @@
 "use strict";
+const myProfile = {
+    name: "Hashir Ahmed K B",
+    age: 24,
+    email: "ahmedhashir96@gmail.com"
+};
+const updateProfile = (input, profileinput) => {
+    const profile = profileinput;
+    console.log("Recieved Profile:", profile);
+    input.name ? profile.name = input.name : input.name;
+    input.age ? profile.age = input.age : input.age;
+    input.email ? profile.email = input.email : input.email;
+    console.log("Upd1ted Profile: ", profile);
+};
+updateProfile({ age: 26 }, myProfile);
+updateProfile({ name: "HashirAKB" }, myProfile);
 function sumOfAge(user1, user2) {
     return user1.age + user2.age;
 }
 ;
+const printUserProfile = (user) => {
+    console.log("The name is:", user.name, "and email is:", user.email);
+};
 // Example usage
 const result = sumOfAge({
     name: "harkirat",
-    age: 20
+    age: 20,
+    email: "email.example.com"
 }, {
     name: "raman",
-    age: 21
+    age: 21,
+    email: "email.example2.com"
 });
-console.log(result); // Output: 41
+printUserProfile({ name: "Hashir", email: "hashir@email.com" });
+console.log(result); // Output: 4
--- week-12/practice/src/index.ts
+++ week-12/practice/src/index.ts
@@ -1,18 +1,52 @@
 interface User {
 	name: string;
 	age: number;
+    email: string,
 }
+
+
+
+type modifyParams = Pick<User, 'name' | 'email' | 'age'>;
+type modifyAgeOrName = Partial<modifyParams>
+
+const myProfile: modifyAgeOrName = {
+    name:"Hashir Ahmed K B",
+    age:24,
+    email:"ahmedhashir96@gmail.com"
+}
+
+const updateProfile = (input: modifyAgeOrName, profileinput: modifyAgeOrName) => {
+    const profile = profileinput;
+    console.log("Recieved Profile:", profile);
+    input.name ? profile.name = input.name : input.name;
+    input.age ? profile.age = input.age : input.age;
+    input.email ? profile.email = input.email : input.email;
+    console.log("Upd1ted Profile: ", profile);
+}
+updateProfile({age:26}, myProfile);
+updateProfile({name:"HashirAKB"}, myProfile);
+
+type userProfile = Pick<User, 'name'| 'email'>;
 
 function sumOfAge(user1: User, user2: User) {
   return user1.age + user2.age;
 };
 
+const printUserProfile = (user: userProfile) => {
+    console.log("The name is:", user.name ,"and email is:", user.email);
+}
+
 // Example usage
 const result = sumOfAge({
 	name: "harkirat",
-	age: 20
+	age: 20,
+    email: "email.example.com"
 }, {
 	name: "raman",
-	age: 21
+	age: 21,
+    email: "email.example2.com"
+
 });
-console.log(result); // Output: 41
+
+printUserProfile({name: "Hashir", email:"hashir@email.com"});
+console.log(result); // Output: 4
