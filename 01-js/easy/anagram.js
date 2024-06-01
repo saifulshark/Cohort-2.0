a34600fc23cdff04c7fe20f84f4371f420cd6bf8
shahilsingh546 <shahilsingh456@gmail.com>
2024-06-01 10:53:43 +0530

    solved the 1st assignment

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,32 @@
 
 function isAnagram(str1, str2) {
 
+if (str1.length !== str2.length)
+  return false;
+
+ str1 = str1.toLowerCase();
+ str2 = str2.toLowerCase();
+ str1 = str1.replace(" ", "");
+ str2 = str2.replace(" ", "");
+ var n1 = str1.length;
+ var n2 = str2.length;
+    let arr = new Array(26).fill(0);
+
+    for (let i = 0; i < n1; i++) {
+        arr[str1.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+    }
+
+    for (let i = 0; i < n2; i++) {
+        arr[str2.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    }
+
+    for (let i = 0; i < 26; i++) {
+        if (arr[i] !== 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 module.exports = isAnagram;
