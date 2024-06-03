d8def926f825b657da80ade3ca27cbef3542ad99
the-osiris <dev.kunal.singh@gmail.com>
2024-06-03 22:46:29 +0530

    easy-1

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,18 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.length != str2.length) return false;
+  const map1 = new Map();
+  for (let i = 0; i < str1.length; i++){
+    const char = str1[i].toLowerCase();
+    map1.set(char, (map1.get(char) || 0) + 1);
+  }
+  for (let i = 0; i < str2.length; i++){
+    const char = str2[i].toLowerCase();
+    if (!map1.has(char) || map1.get(char) === 0) return false;
+    map1.set(char, map1.get(char) - 1);
+  }
+  return true;
 }
 
 module.exports = isAnagram;
