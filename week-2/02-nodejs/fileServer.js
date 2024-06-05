fb5f443d1ca048157067aacce3920cd88be14d1e
Arka Chowdhury <arka.chowdhury.2001@gmail.com>
2024-06-05 18:44:32 +0530

    fileServer Done

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -17,5 +17,33 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.get("/files",(req,res)=>{
+  const path_to_files = path.join(__dirname,"files");
+  fs.readdir(path_to_files,(err,files)=>{
+    if(err){
+      return res.status(500).json({error: 'Failed to retrieve files'});
+    }
+    res.json(files);
+  })
+  
+})
 
+app.get("/file/:filename",(req,res)=>{
+  const file_path = path.join(__dirname,'files',req.params.filename);
+  console.log(file_path);
+  fs.readFile(file_path,"utf-8",(err,data)=>{
+      if(err){
+        return res.status(404).send('File not found');
+        // res.status(404).send('File not found');
+      }
+      res.status(200).send(data);
+      // res.status(200).send(data);
+  })
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
+
+// app.listen(3000);
 module.exports = app;
