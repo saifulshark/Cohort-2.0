86643616926b34c70af82bbdbf999cdce0ac6889
HashirAKB <ahmedhashir96@gmail.com>
2024-06-12 23:18:22 +0530

    Optimal way to use client side rendering.

--- week-14/I_did_all_the_practice_here/hashirs-first-next-app/components/signin.tsx
+++ week-14/I_did_all_the_practice_here/hashirs-first-next-app/components/signin.tsx
@@ -1,8 +1,5 @@
-"use client"
+import { SignInButtonComponent } from "./signinButton";
 export const SignInComponent = () => {
-    function clickHandler() {
-        console.log("Hi there!!!");
-    }
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
         <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
@@ -16,7 +13,7 @@ export const SignInComponent = () => {
                         <LabelledInput label="Username" placeholder="harkirat@gmail.com" />
                         <LabelledInput label="Password" type={"password"} placeholder="123456" />
 
-                        <button onClick={clickHandler} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+                        <SignInButtonComponent/>
 
                     </div>
                 </div>
