d2007f3d209467e72395edcd2bac3aae466e5d89
HashirAKB <ahmedhashir96@gmail.com>
2024-06-12 23:10:30 +0530

    used "use client" for client side rendering of the onclick event.

--- week-14/I_did_all_the_practice_here/hashirs-first-next-app/components/signin.tsx
+++ week-14/I_did_all_the_practice_here/hashirs-first-next-app/components/signin.tsx
@@ -1,5 +1,8 @@
-
+"use client"
 export const SignInComponent = () => {
+    function clickHandler() {
+        console.log("Hi there!!!");
+    }
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
         <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
@@ -12,7 +15,9 @@ export const SignInComponent = () => {
                     <div className="pt-2">
                         <LabelledInput label="Username" placeholder="harkirat@gmail.com" />
                         <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                        <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+
+                        <button onClick={clickHandler} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+
                     </div>
                 </div>
             </a>
