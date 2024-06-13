52a2cdedcc415211581c1e094944abbc2ffa06be
HashirAKB <ahmedhashir96@gmail.com>
2024-06-13 19:16:40 +0530

    ugly way of mapping backend api

--- week-14/I_did_all_the_practice_here/figuring_out_backend_in_next/app/page.tsx
+++ week-14/I_did_all_the_practice_here/figuring_out_backend_in_next/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import axios from 'axios';
 
 async function getUserDetails() {
-  await new Promise((r) => setTimeout(r,5000));
-  const response = await axios.get('https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details');
+  await new Promise((r) => setTimeout(r,2000));
+  const response = await axios.get('http://localhost:3000/api/user');
   return response.data;
 }
 // made this into async component. [This won't work in client side rendering components.]
