51c56cd04f5f8e84deba380230d96b3796c80f0d
HashirAKB <ahmedhashir96@gmail.com>
2024-06-13 20:37:31 +0530

    Understanding POST in nextJS

--- week-14/I_did_all_the_practice_here/figuring_out_backend_in_next/app/api/user/route.tsx
+++ week-14/I_did_all_the_practice_here/figuring_out_backend_in_next/app/api/user/route.tsx
@@ -1,9 +1,22 @@
+import { NextRequest } from "next/server"
+
 export function GET(){
     return Response.json({
         email:"ahmedhashir96@gmail.com",
         name:"Hashirahmed K B"
     })
 }
+
+export async function POST(req: NextRequest){
+    console.log("Hello!!!")
+    const body: {username: string, password: string} = await req.json();
+    console.log(body);
+    return Response.json({
+        msg:"SignUp successfull.",
+        uname: body.username,
+    })
+}
+
 
 // How the above is done on re1ct/express
 // app.get('api/user', (req, res) => {
