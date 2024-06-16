490f25357cb8827e68100d25ac9e075ad9e1ed35
prince981620 <prince981620@gmail.com>
2024-06-17 00:28:26 +0530

    todo easy completed by without using useState

---
+++ week-4/react-basics/todo/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  function createTodo(title,desc){
+    const todocont = document.createElement('div');
+    const todoTitle = document.createElement('h1');
+    todoTitle.textContent = title;
+    const todoDesc = document.createElement('h2');
+    todoDesc.textContent = desc;
+    todocont.appendChild(todoTitle);
+    todocont.appendChild(todoDesc);
+    return todocont;
+  }
+  function addTodo(){
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("desc").value;
+    const todo = createTodo(title,description);
+    console.log(todo);
+    document.getElementById("container").appendChild(createTodo(title,description));
+  }
+  return (
+      <div>
+        <input type="text" placeholder='title' id='title' /> <br />
+        <input type="text" placeholder='description' id='desc' /> <br />
+        <button onClick={addTodo}>Add Todo</button>
+        <div id='container'></div>
+      </div>
+  )
+}
+
+export default App
