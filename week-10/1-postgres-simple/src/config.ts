17a3a48f9b76de758e94594b115c6b078097b4be
adityadewhy <adityadewhy@gmail.com>
2024-06-16 19:27:11 +0530

    started postgress

--- week-10/1-postgres-simple/src/config.ts
+++ week-10/1-postgres-simple/src/config.ts
@@ -1,2 +1,2 @@
 
-export const DB_URL = "postgres://postgres:mysecretpassword@localhost/postgres";
+export const DB_URL = "postgresql://neondb_owner:7JIYFQfjcT1k@ep-bitter-rice-a5br65d9.us-east-2.aws.neon.tech/neondb?sslmode=require";
--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -1,4 +1,4 @@
-import { client } from "..";
+import {client} from "../index";
 
 /*
  * Should insert into the users table
@@ -9,7 +9,21 @@ import { client } from "..";
  *   name: string
  * }
  */
-export async function createUser(username: string, password: string, name: string) {
+interface User{
+    username:string;
+    password:string;
+    name:string
+}
+export async function createUser(
+	username: string,
+	password: string,
+	name: string
+) {
+	const syntax = 'INSERT INTO users(username,password,name) VALUES($1,$2)'
+    const values = [username,password,name]
+    const res = await client.query(syntax,values)
+    const user:User = res[0];
+
     
 }
 
@@ -22,5 +36,15 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+	// return {
+	// 	username: await client.query(`
+    //             SELECT username FROM users where userID="userID"
+    //             `),
+	// 	password: await client.query(`
+    //             SELECT password FROM users where userID="userID"
+    //             `),
+	// 	name: await client.query(`
+    //             SELECT name FROM users where userID="userID"
+    //             `),
+	// };
 }
