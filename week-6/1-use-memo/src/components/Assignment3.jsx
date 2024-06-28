a32a3d050372f90db92146ed70516e9200fa76b5
prince981620 <prince981620@gmail.com>
2024-06-28 14:11:28 +0530

    solved 1-usememo -> Assignment3

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -12,7 +12,13 @@ export const Assignment3 = () => {
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    let totalValue = useMemo(()=>{
+        let value = 0;
+        for(let i=0;i<items.length;i++){
+            value += items[i].value;
+        }
+        return value;
+    },items)
     // Your code ends here
     return (
         <div>
