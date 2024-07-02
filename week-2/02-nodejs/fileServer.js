b1bcd8339699ac1fc4890045aeaad3be6485f27f
nitheesh5601 <nitheeshwaran56@gmail.com>
2024-07-02 07:45:18 +0530

    week 2 file server assignments completed

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -16,6 +16,31 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const port = 3000;
+
+app.get('/files',function(req,res){
+  fs.readdir(path.join(__dirname,'./files/'),(err,files)=>{
+    if(err){
+      return res.status(500).json({error:'500 Internal error'})
+    }
+    res.json(files)
+  })
+})
+
+app.get('/files/:filename',function(req,res){
+  const filepath = path.join(__dirname,'./files/',req.params.filename)
+
+  fs.readFile(filepath,'utf-8',(err,data)=>{
+  if(err){
+    return res.status(404).send('File not found');
+  }
+  res.send(data);
+  });
+});
+
+
+
+app.listen(port);
 
 
 module.exports = app;
