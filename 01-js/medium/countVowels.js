28ef16092d22c0aa5a802fbe67cab7218027313d
Yash Gandhi <yashgandhi.2408@gmail.com>
2024-07-01 20:52:41 -0400

    count-vowels-solved

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -7,6 +7,15 @@
 
 function countVowels(str) {
     // Your code here
+    let allVowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+    let count = 0;
+
+    for(let char of str){
+      if(allVowels.includes(char)){
+        count += 1
+      }
+    }
+    return count;
 }
 
 module.exports = countVowels;
