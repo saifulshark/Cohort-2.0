a5cb3bf59b71b3265bcae2b9c954bb1614ec8e94
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2024-07-03 17:38:05 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -4,33 +4,33 @@
   transactions is an array where each
   Transaction - an object like 
         {
-    id: 1,
-    timestamp: 1656076800000,
-    price: 10,
-    category: 'Food',
-    itemName: 'Pizza',
-  }
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Pizza',
+	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  const totals = {};
-
-  transactions.forEach(transaction => {
-    const category = transaction.category;
-    const price = transaction.price;
-
-    if (!totals[category]) {
-      totals[category] = price;
-    } else {
-      totals[category] += price;
-    }
-  });
-
-  const result = [];
-  for (const category in totals) {
-    result.push({ category: category, totalSpent: totals[category] });
+ 
+  const catMap ={};
+  
+  for(const transaction of transactions ){
+   
+     const{category,price}=transaction;
+     
+     if(!catMap[category]){
+      catMap[category]=0;
+     }
+     catMap[category]+=price;
   }
+  const result = Object.keys(catMap).map(category=>({
+       category :category,
+       totalSpent: catMap[category]
+  }));
+
   return result;
 }
 
