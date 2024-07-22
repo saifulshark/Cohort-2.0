442856da278d8ee3169efbad552710d61a2f277c
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2024-07-22 11:27:27 +0530

    halfway assignment done

--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -39,11 +39,40 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-  const express = require('express');
-  const bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+let todos = [];
+app.get('/todos', (req, res) => {
+
+  res.json(todos)
+
+})
+
+app.get('/todos/:id',(req,res)=>{
+  const todo=todos.find(t=>t.id===parseInt(req.params.id));
+  if(!todo){
+    res.status(404).send();
+  }
+  else{
+    res.json(todo);
+  }
   
-  const app = express();
-  
-  app.use(bodyParser.json());
-  
-  module.exports = app;
+});
+
+app.post('/todos',(req,res)=>{
+  const newTodo={
+    id:Math.floor(Math.random() * 10000),
+    title:req.body.title,
+    description:req.body.description
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+})
+
+
+module.exports = app;
