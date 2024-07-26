b83e011a883748b635c1b6b8ae8009d3ff0d5683
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2024-07-26 11:47:14 +0530

    database created

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -6,14 +6,22 @@ mongoose.connect('your-mongodb-url');
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
