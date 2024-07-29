35220fa4f022e8b830adf8015e1529a02c9031bb
Rohit kumar <rohitkumarrr2022@gmail.com>
2024-07-29 22:37:31 +0530

    Learned something new 'Zod'

--- Expressjs/http.js
+++ Expressjs/http.js
@@ -397,76 +397,116 @@
 
 //Jwt lessons
 
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const jwtPassword = "123456";
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const jwtPassword = "123456";
 
+// const app = express();
+
+// const ALL_USERS = [
+//   {
+//     username: "harkirat@gmail.com",
+//     password: "123",
+//     name: "harkirat singh",
+//   },
+//   {
+//     username: "raman@gmail.com",
+//     password: "123321",
+//     name: "Raman singh",
+//   },
+//   {
+//     username: "priya@gmail.com",
+//     password: "123321",
+//     name: "Priya kumari",
+//   },
+// ];
+
+// function userExists(username, password) {
+//   // write logic to return true or false if this user exists
+//   // in ALL_USERS array
+//   let userExists = false;
+//   for(let i=0; i<ALL_USERS.length; i++)
+//   {
+//     if(ALL_USERS[i].username === username && ALL_USERS[i].password === password){
+//       userExists = true;
+//     }
+//   }
+//   return userExists;
+// }
+// app.use(express.json());
+// app.post("/signin", function (req, res) {
+//   const username = req.body.username;
+//   const password = req.body.password;
+
+//   if (!userExists(username, password)) {
+//     return res.status(403).json({
+//       msg: "User doesnt exist in our in memory db",
+//     });
+//   }
+
+//   var token = jwt.sign({ username: username }, jwtPassword);
+//   return res.json({
+//     token,
+//   });
+// });
+
+// app.get("/users", function (req, res) {
+//   const token = req.headers.authorization;
+
+//   const decoded = jwt.verify(token, jwtPassword);
+//   const username = decoded.username;
+//   res.json({
+//     users: ALL_USERS.filter((value) => {
+//       if(value.username === username)
+//       {
+//         return false;
+//       }
+//       else 
+//       {
+//         return true;
+//       }
+//     })
+//   });
+// });
+
+// app.listen(3000);
+const express = require('express');
+const zod = require('zod');
 const app = express();
 
-const ALL_USERS = [
-  {
-    username: "harkirat@gmail.com",
-    password: "123",
-    name: "harkirat singh",
-  },
-  {
-    username: "raman@gmail.com",
-    password: "123321",
-    name: "Raman singh",
-  },
-  {
-    username: "priya@gmail.com",
-    password: "123321",
-    name: "Priya kumari",
-  },
-];
 
-function userExists(username, password) {
-  // write logic to return true or false if this user exists
-  // in ALL_USERS array
-  let userExists = false;
-  for(let i=0; i<ALL_USERS.length; i++)
-  {
-    if(ALL_USERS[i].username === username && ALL_USERS[i].password === password){
-      userExists = true;
-    }
-  }
-  return userExists;
-}
+const schema = zod.array(zod.number());
+
+
+// {
+//   "email": string -> email 
+//   "password" : atleast 8 characters required 
+//   "country" : "IN" OR "US"
+// }
+
+const schemaa = zod.object({
+  email : zod.string(),
+  password : zod.string(),
+  country : zod.literal('IN').or(zod.literal('US')),
+  kidneys : zod.array(zod.number()) 
+})
+
+
 app.use(express.json());
-app.post("/signin", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  if (!userExists(username, password)) {
-    return res.status(403).json({
-      msg: "User doesnt exist in our in memory db",
-    });
-  }
-
-  var token = jwt.sign({ username: username }, jwtPassword);
-  return res.json({
-    token,
-  });
+app.post("/health-checkup", (req,res) => {
+  const kidneys = req.body.kidneys;
+  const kidneyLength = kidneys.length;
+  const response = schema.safeParse(kidneys);
+  res.send({
+    response
+  })
 });
 
-app.get("/users", function (req, res) {
-  const token = req.headers.authorization;
-
-  const decoded = jwt.verify(token, jwtPassword);
-  const username = decoded.username;
+app.use((err,req,res,next) => {
   res.json({
-    users: ALL_USERS.filter((value) => {
-      if(value.username === username)
-      {
-        return false;
-      }
-      else 
-      {
-        return true;
-      }
-    })
-  });
+    msg : "Sorry something is up with the server"
+  })
 });
 
 app.listen(3000);
