7e39103f156e21f782d35f81415ee8c5f6c8fdc5
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2024-07-31 16:20:11 +0530

    jwt added

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,16 +1,32 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
-
+const jwt = require("jwt");
+const JWT_SECRET = require("../index");
+const { User } = require("../solution/db");
 // Admin Routes
 router.post('/signup', (req, res) => {
     // Implement admin signup logic
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async(req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    const isvalid = await Userfind({
+        username, password
+    })
+    if (isvalid) {
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+    }
+    else {
+        res.status(411).json({
+            msg: "not the user"
+        })
+    }
 });
-
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
 });
