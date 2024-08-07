367f70c5263330a82be738c85b745c6d6fa5cf10
Pavan Bhat <pavanksp612@gmail.com>
2024-08-08 00:04:09 +0530

    completed js foundations assignments

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,16 @@
 */
 
 function countVowels(str) {
-    // Your code here
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let vowelCount = 0;
+
+    for(let i=0; i<str.length; i++) {
+      if(vowels.includes(str[i].toLowerCase())) {
+        vowelCount++;
+      }
+    }
+
+    return vowelCount;
 }
 
 module.exports = countVowels;
