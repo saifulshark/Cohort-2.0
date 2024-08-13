cfd253c2328954798b5e0d4a8012d8a8d59199a0
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-08-13 10:55:56 +0530

    done with Middlewares

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const assert = require('assert');
 const express = require('express');
+const { count } = require('console');
 
 const app = express();
 let requestCount = 0;
@@ -10,12 +11,17 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
+app.use((req, res, next)=>{
+  requestCount = requestCount + 1;
+  next();
+}) 
+
 app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+  res.status(200).json({ name: 'john' , count: requestCount });
 });
 
 app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+  res.status(200).json({ msg: 'created dummy user', count: requestCount });
 });
 
 app.get('/requestCount', function(req, res) {
