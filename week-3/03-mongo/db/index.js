c1e89d732d3132be218b7a0523de8f00777eb17c
HAROON NISSAR KHANDAY <136074700+Khandayharoon@users.noreply.github.com>
2024-08-17 00:49:44 +0530

    Done MongoDB

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { type } = require('os');
+const { title } = require('process');
 
 // Connect to MongoDB
 mongoose.connect('your-mongodb-url');
@@ -6,14 +8,29 @@ mongoose.connect('your-mongodb-url');
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username : String(),
+    email:String(),
+    password:String()
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username : String(),
+    email:String(),
+    password:String(),
+    purchasedCourses: [{
+        type:mongoose.Schema.Types.ObjectId,
+        reference : `Course`,
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+     title:String(),
+     description : String(),
+     imageLink: String,
+     price :Number,
+
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
