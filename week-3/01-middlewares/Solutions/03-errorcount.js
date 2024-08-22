7fddc0174dee8a7d2744bdd7b044dd1475c16627
Aman254 <aman.mishra.2005@gmail.com>
2024-08-22 15:43:39 +0530

    Added Solutions for 01 Middlewares 01 and 03

--- week-3/01-middlewares/Solutions/03-errorcount.js
+++ week-3/01-middlewares/Solutions/03-errorcount.js
@@ -12,7 +12,7 @@ let errorCount = 0;
 
 app.get("/user", function (req, res) {
   throw new Error("User not found");
-  res.status(200).json({ name: "john" });
+  // res.status(200).json({ name: "john" });
 });
 
 app.post("/user", function (req, res) {
@@ -21,6 +21,14 @@ app.post("/user", function (req, res) {
 
 app.get("/errorCount", function (req, res) {
   res.status(200).json({ errorCount });
+});
+
+app.use((err, req, res, next) => {
+  res.status(404).json({
+    status: err.status || 404,
+    msg: err.message || "An Error Occured.",
+  });
+  errorCount++;
 });
 
 module.exports = app;
