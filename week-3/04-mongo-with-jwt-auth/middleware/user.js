0526cb75bf25a9f23199149cedf913c6752751c0
Subrata Kundu <kundu.subrata2020@outlook.com>
2024-09-04 12:23:47 +0530

    solved week3 04-mongo-with-jwt assignment

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,31 @@
+const jwt = require("jsonwebtoken");
+const secret = require("../config.env");
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  try {
+    const auth = req.headers.authorization;
+    const authArr = auth.split(" ");
+    const token = authArr[1];
+
+    const decoded = jwt.verify(token, secret);
+
+    if (decoded.username) {
+      req.username = decoded.username;
+      next();
+    } else {
+      res.status(403).json({
+        msg: "you are not authenticated",
+      });
+    }
+  } catch (e) {
+    res.status(404).json({
+      msg: "middleware error",
+      error: e.stack,
+    });
+  }
 }
 
 module.exports = userMiddleware;
+
--- week-3/04-mongo-with-jwt-auth/routes/user.js
+++ week-3/04-mongo-with-jwt-auth/routes/user.js
@@ -1,26 +1,120 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
-
+const { User, Course } = require("../db");
+const secret = require("../config.env");
+const jwt = require("jsonwebtoken");
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await User.create({
+      username,
+      password,
+    });
+    res.status(201).json({
+      msg: "User created successfully",
+    });
+  } catch (e) {
+    res.status(404).json({
+      msg: "signup error",
+    });
+  }
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", async (req, res) => {
+  // Implement admin signup logic
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const find = User.findOne({
+      username,
+      password,
+    });
+
+    if (find) {
+      const token = jwt.sign({ username }, secret);
+      res.status(200).json({
+        token: token,
+      });
+    } else {
+      res.status(404).json({
+        msg: "User doesn't exist",
+      });
+    }
+  } catch (e) {
+    res.status(404).json({
+      msg: "sigin error",
+      error: e.stack,
+    });
+  }
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  try {
+    const courses = await Course.find({});
+    res.status(200).json({
+      courses: courses,
+    });
+  } catch (e) {
+    res.status(404).json({
+      msg: "courses error",
+    });
+  }
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+  try {
+    const username = req.username;
+    const courseId = req.params.courseId;
+
+    await User.updateOne(
+      {
+        username,
+      },
+      {
+        $push: {
+          purchasedCourses: courseId,
+        },
+      },
+    );
+    res.status(201).json({
+      msg: "Course purchased successfully",
+    });
+  } catch (e) {
+    res.status(404).json({
+      msg: "purchase error",
+    });
+  }
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  try {
+    const username = req.username;
+    const user = await User.findOne({
+      username,
+    });
+    const courses = await Course.find({
+      _id: {
+        $in: user.purchasedCourses,
+      },
+    });
+    res.status(200).json({
+      courses: courses,
+    });
+  } catch (e) {
+    res.status(404).json({
+      msg: "fetching course error",
+    });
+  }
 });
 
-module.exports = router
+module.exports = router;
+
